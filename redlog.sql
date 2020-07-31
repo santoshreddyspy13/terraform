b566dea941e91545017e35fc4237542fd24bbe2d
santoshreddyspy13 <69038787+santoshreddyspy13@users.noreply.github.com>
2020-07-31 16:11:21 +0530

    Add files via upload

---
+++ redlog.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 30, 2020 at 10:01 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `redlog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `red`
+--
+
+CREATE TABLE `red` (
+  `id` int(11) NOT NULL,
+  `full_name` text NOT NULL,
+  `email` text NOT NULL,
+  `pass` text NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `red`
+--
+
+INSERT INTO `red` (`id`, `full_name`, `email`, `pass`, `status`) VALUES
+(1, 'Avinash Srinivas', 'avinash.srinivas@mindtree.com', 'X2vsiq', 0),
+(3, 'Santosh Reddy N', 'santosh.n@mindtree.com', 'Test@123', 1),
+(4, 'Ancy Alexander', 'ancy.alexander@mindtree.com', 'Ancy@123', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `red`
+--
+ALTER TABLE `red`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `red`
+--
+ALTER TABLE `red`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
